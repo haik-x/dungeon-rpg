49c03745a943a23bb4dd55f62825cb40981a5205
Benjamin Vergara <53795904+haik-x@users.noreply.github.com>
2021-12-22 01:10:23 -0600

    player movement

---
+++ 2d-rpg/Assets/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x, y, 0);
+
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        transform.Translate(moveDelta * Time.deltaTime);
+    }
+    
+}
